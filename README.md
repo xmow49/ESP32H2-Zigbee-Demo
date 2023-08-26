064aca4a0a6d7a17b297c93f045785af00bac9a0
Xmow49 <dorian.benech007@gmail.com>
2023-08-26 17:15:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ESP32-H2 Demo Zigbee Device
+
+This project is a demo Zigbee device based on the ESP32-H2 / ESP32-C6
+
+## Features
+
+- [x] DHT22 temperature and humidity sensor
+- [x] Binary switch
+- [x] Binary input
+
+![Alt text](image.png)
+
+## Hardware
+
+- [ESP32-H2](https://www.espressif.com/en/products/socs/h2/overview)
+
+## GPIO
+
+| GPIO   | Function              |
+| ------ | --------------------- |
+| GPIO8  | DHT22 data            |
+| GPIO3  | Binary output (LED)   |
+| GPIO12 | Binary input (Button) |
---
+++ main/esp_zb_light.c
@@ -0,0 +1,257 @@
+#include "nvs_flash.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "ha/esp_zigbee_ha_standard.h"
+#include "esp_zb_light.h"
+#include "string.h"
+#include "driver/gpio.h"
+#include "DHT22.h"
+
+static const char *TAG = "LIGHT";
+/********************* Define functions **************************/
+void reportAttribute(uint8_t endpoint, uint16_t clusterID, uint16_t attributeID, void *value, uint8_t value_length)
+{
+    esp_zb_zcl_report_attr_cmd_t cmd = {
+        .zcl_basic_cmd = {
+            .dst_addr_u.addr_short = 0x0000,
+            .dst_endpoint = endpoint,
+            .src_endpoint = endpoint,
+        },
+        .address_mode = ESP_ZB_APS_ADDR_MODE_16_ENDP_PRESENT,
+        .clusterID = clusterID,
+        .attributeID = attributeID,
+        .cluster_role = ESP_ZB_ZCL_CLUSTER_SERVER_ROLE,
+    };
+    esp_zb_zcl_attr_t *value_r = esp_zb_zcl_get_attribute(endpoint, clusterID, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE, attributeID);
+    memcpy(value_r->data_p, value, value_length);
+    esp_zb_zcl_report_attr_cmd_req(&cmd);
+}
+void button_task(void *pvParameters)
+{
+    uint8_t last_state = 0;
+    while (1)
+    {
+        uint8_t button_state = !gpio_get_level(GPIO_NUM_12);
+        if (button_state != last_state)
+        {
+            ESP_LOGI(TAG, "Button changed: %d", button_state);
+            last_state = button_state;
+            reportAttribute(HA_ESP_LIGHT_ENDPOINT, ESP_ZB_ZCL_CLUSTER_ID_BINARY_INPUT, ESP_ZB_ZCL_ATTR_BINARY_INPUT_PRESENT_VALUE_ID, &button_state, 1);
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+}
+
+void dht22_task(void *pvParameters)
+{
+    while (1)
+    {
+        setDHTgpio(GPIO_NUM_8);
+        int ret = readDHT();
+        if (ret != DHT_OK)
+            errorHandler(ret);
+        else
+        {
+            ESP_LOGI(TAG, "Hum: %.1f Tmp: %.1f\n", getHumidity(), getTemperature());
+            uint16_t temperature = (uint16_t)(getTemperature() * 100);
+            uint16_t humidity = (uint16_t)(getHumidity() * 100);
+            reportAttribute(HA_ESP_LIGHT_ENDPOINT, ESP_ZB_ZCL_CLUSTER_ID_TEMP_MEASUREMENT, ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_VALUE_ID, &temperature, 2);
+            reportAttribute(HA_ESP_LIGHT_ENDPOINT, ESP_ZB_ZCL_CLUSTER_ID_REL_HUMIDITY_MEASUREMENT, ESP_ZB_ZCL_ATTR_REL_HUMIDITY_MEASUREMENT_VALUE_ID, &humidity, 2);
+        }
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void bdb_start_top_level_commissioning_cb(uint8_t mode_mask)
+{
+    ESP_ERROR_CHECK(esp_zb_bdb_start_top_level_commissioning(mode_mask));
+}
+
+static esp_err_t attr_cb(const esp_zb_zcl_set_attr_value_message_t message)
+{
+    esp_err_t ret = ESP_OK;
+    bool light_state = 0;
+    if (message.info.status == ESP_ZB_ZCL_STATUS_SUCCESS)
+    {
+        ESP_LOGI(TAG, "Received message: endpoint(0x%x), cluster(0x%x), attribute(0x%x), data size(%d)", message.info.dst_endpoint,
+                 message.info.cluster, message.attribute, message.data.size);
+        if (message.info.dst_endpoint == HA_ESP_LIGHT_ENDPOINT)
+        {
+            if (message.info.cluster == ESP_ZB_ZCL_CLUSTER_ID_ON_OFF)
+            {
+                if (message.attribute == ESP_ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID && message.data.type == ESP_ZB_ZCL_ATTR_TYPE_BOOL)
+                {
+                    light_state = message.data.value ? *(bool *)message.data.value : light_state;
+                    gpio_set_direction(GPIO_NUM_3, GPIO_MODE_OUTPUT);
+                    gpio_set_level(GPIO_NUM_3, light_state);
+                    ESP_LOGI(TAG, "Light sets to %s", light_state ? "On" : "Off");
+                }
+            }
+        }
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Received message: status(%d) error", message.info.status);
+        ret = ESP_ERR_INVALID_ARG;
+    }
+    return ret;
+}
+
+void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct)
+{
+    uint32_t *p_sg_p = signal_struct->p_app_signal;
+    esp_err_t err_status = signal_struct->esp_err_status;
+    esp_zb_app_signal_type_t sig_type = *p_sg_p;
+    switch (sig_type)
+    {
+    case ESP_ZB_ZDO_SIGNAL_SKIP_STARTUP:
+        ESP_LOGI(TAG, "Zigbee stack initialized");
+        esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_INITIALIZATION);
+        break;
+    case ESP_ZB_BDB_SIGNAL_DEVICE_FIRST_START:
+    case ESP_ZB_BDB_SIGNAL_DEVICE_REBOOT:
+        if (err_status == ESP_OK)
+        {
+            ESP_LOGI(TAG, "Start network steering");
+            esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_STEERING);
+        }
+        else
+        {
+            /* commissioning failed */
+            ESP_LOGW(TAG, "Failed to initialize Zigbee stack (status: %s)", esp_err_to_name(err_status));
+        }
+        break;
+    case ESP_ZB_BDB_SIGNAL_STEERING:
+        if (err_status == ESP_OK)
+        {
+            esp_zb_ieee_addr_t extended_pan_id;
+            esp_zb_get_extended_pan_id(extended_pan_id);
+            ESP_LOGI(TAG, "Joined network successfully (Extended PAN ID: %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x, PAN ID: 0x%04hx, Channel:%d)",
+                     extended_pan_id[7], extended_pan_id[6], extended_pan_id[5], extended_pan_id[4],
+                     extended_pan_id[3], extended_pan_id[2], extended_pan_id[1], extended_pan_id[0],
+                     esp_zb_get_pan_id(), esp_zb_get_current_channel());
+            xTaskCreate(button_task, "button_task", 4096, NULL, 5, NULL);
+            xTaskCreate(dht22_task, "dht22_task", 4096, NULL, 5, NULL);
+        }
+        else
+        {
+            ESP_LOGI(TAG, "Network steering was not successful (status: %s)", esp_err_to_name(err_status));
+            esp_zb_scheduler_alarm((esp_zb_callback_t)bdb_start_top_level_commissioning_cb, ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "ZDO signal: %s (0x%x), status: %s", esp_zb_zdo_signal_to_string(sig_type), sig_type,
+                 esp_err_to_name(err_status));
+        break;
+    }
+}
+
+static void esp_zb_task(void *pvParameters)
+{
+    /* initialize Zigbee stack with Zigbee end-device config */
+    esp_zb_cfg_t zb_nwk_cfg = ESP_ZB_ZED_CONFIG();
+    esp_zb_init(&zb_nwk_cfg);
+
+    // ------------------------------ Cluster BASIC ------------------------------
+    esp_zb_basic_cluster_cfg_t basic_cluster_cfg = {
+        .zcl_version = ESP_ZB_ZCL_BASIC_ZCL_VERSION_DEFAULT_VALUE,
+        .power_source = 0x03,
+    };
+    uint32_t ApplicationVersion = 0x0001;
+    uint32_t StackVersion = 0x0002;
+    uint32_t HWVersion = 0x0002;
+    uint8_t ManufacturerName[] = {14, 'G', 'a', 'm', 'm', 'a', 'T', 'r', 'o', 'n', 'i', 'q', 'u', 'e', 's'};
+    uint8_t ModelIdentifier[] = {4, 'D', 'e', 'm', 'o'};
+    uint8_t DateCode[] = {8, '2', '0', '2', '3', '0', '8', '2', '6'};
+    esp_zb_attribute_list_t *esp_zb_basic_cluster = esp_zb_basic_cluster_create(&basic_cluster_cfg);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_APPLICATION_VERSION_ID, &ApplicationVersion);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_STACK_VERSION_ID, &StackVersion);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_HW_VERSION_ID, &HWVersion);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_MANUFACTURER_NAME_ID, ManufacturerName);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_MODEL_IDENTIFIER_ID, ModelIdentifier);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_DATE_CODE_ID, DateCode);
+
+    // ------------------------------ Cluster IDENTIFY ------------------------------
+    esp_zb_identify_cluster_cfg_t identify_cluster_cfg = {
+        .identify_time = 0,
+    };
+    esp_zb_attribute_list_t *esp_zb_identify_cluster = esp_zb_identify_cluster_create(&identify_cluster_cfg);
+
+    // ------------------------------ Cluster SWITCH ------------------------------
+    esp_zb_groups_cluster_cfg_t groups_cluster_cfg = {
+        .groups_name_support_id = 0,
+    };
+    esp_zb_attribute_list_t *esp_zb_groups_cluster = esp_zb_groups_cluster_create(&groups_cluster_cfg);
+
+    esp_zb_scenes_cluster_cfg_t scenes_cluster_cfg = {
+        .name_support = 0,
+    };
+    esp_zb_attribute_list_t *esp_zb_scenes_cluster = esp_zb_scenes_cluster_create(&scenes_cluster_cfg);
+
+    esp_zb_on_off_cluster_cfg_t on_off_cfg = {
+        .on_off = 0,
+    };
+    esp_zb_attribute_list_t *esp_zb_on_off_cluster = esp_zb_on_off_cluster_create(&on_off_cfg);
+
+    // ------------------------------ Cluster BINARY INPUT ------------------------------
+    esp_zb_binary_input_cluster_cfg_t binary_input_cfg = {
+        .out_of_service = 0,
+        .status_flags = 0,
+    };
+    uint8_t present_value = 0;
+    esp_zb_attribute_list_t *esp_zb_binary_input_cluster = esp_zb_binary_input_cluster_create(&binary_input_cfg);
+    esp_zb_binary_input_cluster_add_attr(esp_zb_binary_input_cluster, ESP_ZB_ZCL_ATTR_BINARY_INPUT_PRESENT_VALUE_ID, &present_value);
+
+    // ------------------------------ Cluster Temperature ------------------------------
+    esp_zb_temperature_meas_cluster_cfg_t temperature_meas_cfg = {
+        .measured_value = 0xFFFF,
+        .min_value = -50,
+        .max_value = 100,
+    };
+    esp_zb_attribute_list_t *esp_zb_temperature_meas_cluster = esp_zb_temperature_meas_cluster_create(&temperature_meas_cfg);
+
+    // ------------------------------ Cluster Humidity ------------------------------
+    esp_zb_humidity_meas_cluster_cfg_t humidity_meas_cfg = {
+        .measured_value = 0xFFFF,
+        .min_value = 0,
+        .max_value = 100,
+    };
+    esp_zb_attribute_list_t *esp_zb_humidity_meas_cluster = esp_zb_humidity_meas_cluster_create(&humidity_meas_cfg);
+
+    // ------------------------------ Create cluster list ------------------------------
+    esp_zb_cluster_list_t *esp_zb_cluster_list = esp_zb_zcl_cluster_list_create();
+    esp_zb_cluster_list_add_basic_cluster(esp_zb_cluster_list, esp_zb_basic_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_identify_cluster(esp_zb_cluster_list, esp_zb_identify_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_groups_cluster(esp_zb_cluster_list, esp_zb_groups_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_scenes_cluster(esp_zb_cluster_list, esp_zb_scenes_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_on_off_cluster(esp_zb_cluster_list, esp_zb_on_off_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_binary_input_cluster(esp_zb_cluster_list, esp_zb_binary_input_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_temperature_meas_cluster(esp_zb_cluster_list, esp_zb_temperature_meas_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_humidity_meas_cluster(esp_zb_cluster_list, esp_zb_humidity_meas_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+
+    // ------------------------------ Create endpoint list ------------------------------
+    esp_zb_ep_list_t *esp_zb_ep_list = esp_zb_ep_list_create();
+    esp_zb_ep_list_add_ep(esp_zb_ep_list, esp_zb_cluster_list, HA_ESP_LIGHT_ENDPOINT, ESP_ZB_AF_HA_PROFILE_ID, ESP_ZB_HA_IAS_ZONE_ID);
+
+    // ------------------------------ Register Device ------------------------------
+    esp_zb_device_register(esp_zb_ep_list);
+    esp_zb_device_add_set_attr_value_cb(attr_cb);
+    esp_zb_set_primary_network_channel_set(ESP_ZB_PRIMARY_CHANNEL_MASK);
+
+    ESP_ERROR_CHECK(esp_zb_start(false));
+    esp_zb_main_loop_iteration();
+}
+
+void app_main(void)
+{
+    esp_zb_platform_config_t config = {
+        .radio_config = ESP_ZB_DEFAULT_RADIO_CONFIG(),
+        .host_config = ESP_ZB_DEFAULT_HOST_CONFIG(),
+    };
+    ESP_ERROR_CHECK(nvs_flash_init());
+    /* load Zigbee light_bulb platform config to initialization */
+    ESP_ERROR_CHECK(esp_zb_platform_config(&config));
+    /* hardware related and device init */
+    xTaskCreate(esp_zb_task, "Zigbee_main", 4096, NULL, 5, NULL);
+}
